953fa5da3fd765d64955a0d00d72493f2a350119
Alex A. Kwashie <alex.kwashie@hotmail.com>
2018-12-10 22:14:33 +0000

    Working with Postgre

--- app.py
+++ app.py
@@ -25,6 +25,10 @@ window.mainloop()
 
 '''
 
+#####################################
+#SQLITE
+#####################################
+'''
 import sqlite3
 
 def create_table():
@@ -71,6 +75,50 @@ update(48,5.3 , 'bags')
 
 print(view())
 
+'''
+
+import psycopg2
+
+def create_table():
+    conn = psycopg2.connect("dbname = 'database1' user = 'postgres' password = 'pan' host='localhost' port = '5432'")
+    cur = conn.cursor() #cursor object allows you to select rows
+    cur.execute("CREATE TABLE IF NOT EXISTS store(item TEXT, Quantity INTEGER, Price REAL)")
+    conn.commit()
+    conn.close()
 
 
+def insert(item, quantity, price):
+    conn = psycopg2.connect("dbname = 'database1' user = 'postgres' password = 'pan' host='localhost' port = '5432'")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO store VALUES(%s,%s,%s)",(item, quantity, price)) #pass variable as a 2nd para to avoid sql injecion
+    conn.commit()
+    conn.close()
 
+
+def view():
+    conn = psycopg2.connect("dbname = 'database1' user = 'postgres' password = 'pan' host='localhost' port = '5432'")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM store")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+def delete(item):
+    conn = psycopg2.connect("dbname = 'database1' user = 'postgres' password = 'pan' host='localhost' port = '5432'")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM store WHERE item = ?",[item])
+    conn.commit()
+    conn.close()
+
+def update(quantity, price,item):
+    conn = psycopg2.connect("dbname = 'database1' user = 'postgres' password = 'pan' host='localhost' port = '5432'")
+    cur = conn.cursor()
+    cur.execute("UPDATE store SET quantity = %s, price =%s  WHERE item = %s", (quantity, price, item))
+    conn.commit()
+    conn.close()
+
+
+create_table()
+
+#update(29, 34.89,'Mouse')
+print(view())
