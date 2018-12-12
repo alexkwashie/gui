7be9dc830b08a73f21b9844162efb4e2a6505017
Alex A. Kwashie <alex.kwashie@hotmail.com>
2018-12-12 16:49:12 +0000

    add backend code to script file

--- script.py
+++ script.py
@@ -1,4 +1,20 @@
 from tkinter import *
+import backend
+
+def veiw_command():
+    list1.delete(0,END)#this is to delete everthing from index of 0 to the end: stops repitition
+    for row in backend.view():
+        list1.insert(END,row)#END is used to put the others at the end of the first one
+
+def search_command():
+    list1.delete(0,END)
+    for row in backend.search(title_text.get(),Auth_text.get(),year_text.get(), isb_text.get()):
+        list1.insert(END,row)
+
+def insert_command():
+    backend.insert(title_text.get(),Auth_text.get(),year_text.get(), isb_text.get())
+    list1.delete(0,END)
+    list1.insert(END,(title_text.get(),Auth_text.get(),year_text.get(), isb_text.get()))
 
 window = Tk()
 
@@ -41,13 +57,13 @@ list1.configure(yscrollcommand = sb1.set)
 sb1.configure(command=list1.yview)
 
 
-b1=Button(window,text="View all", width=12)
+b1=Button(window,text="View all", width=12, command = veiw_command)
 b1.grid(row=2,column=3)
 
-b2=Button(window,text="Search entry", width=12)
+b2=Button(window,text="Search entry", width=12, command = search_command)
 b2.grid(row=3,column=3)
 
-b3=Button(window,text="Add entry", width=12)
+b3=Button(window,text="Add entry", width=12, command = insert_command)
 b3.grid(row=4,column=3)
 
 b4=Button(window,text="Update selected", width=12)
