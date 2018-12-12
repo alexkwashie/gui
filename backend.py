1639ca5fe7bb2a82fb5a8cf8488eeae2df8dc33f
Alex A. Kwashie <alex.kwashie@hotmail.com>
2018-12-12 14:51:04 +0000

    Building Gui backend

--- backend.py
+++ backend.py
@@ -23,10 +23,35 @@ def view():
     conn.close()
     return rows
 
+def search(title="", Author="", year="", isbn=""):
+    # set default input as empty strings
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM books WHERE title = ? OR Author = ? OR  year=? OR  isbn=?", (title, Author, year, isbn))
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+def delete(id):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM books WHERE id = ?", (id,))
+    conn.commit()
+    conn.close()
+
+def update(title, Author, year, isbn):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("UPDATE books SET Author = ?,  year=?,  isbn=? WHERE title = ?", ( Author, year, isbn, title))
+    conn.commit()
+    conn.close()
+
 
 connect()
 
-insert('mong jim', 'jonah hill', 1528, 87582925)
-
+#insert('New book', 'Manbook', 2018, 2789429033)
+#print(search(Author="ken smith"))
+update('New book', 'Unk jim', 1983, 3789429033)
+#delete(2)
 print(view())
 
